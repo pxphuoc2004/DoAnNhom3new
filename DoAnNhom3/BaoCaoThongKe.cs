e175a101357038364acd79b1a537c68f97b8ac48
NXNL <400bom@gmail.com>
2025-07-22 14:55:09 +0700

    1

--- DoAnNhom3/BaoCaoThongKe.cs
+++ DoAnNhom3/BaoCaoThongKe.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DoAnNhom3.QuanLy;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +20,53 @@ namespace DoAnNhom3
         }
 
         private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadBaoCao()
+        {
+            string query = "";
+            SqlParameter[] parameters = {
+            new SqlParameter("@TuNgay", dtpTuNgay.Value.Date),
+            new SqlParameter("@DenNgay", dtpDenNgay.Value.Date)
+            };
+
+            if (rdoNgay.Checked)
+            {
+                query = @"
+            SELECT MaBaoCaoNgay, NgayThang, TenMonAn, DonViTinh, SoLuong, DoanhThuNgay 
+            FROM BaoCaoNgay
+            WHERE NgayThang BETWEEN @TuNgay AND @DenNgay";
+            }
+            else if (rdoThang.Checked)
+            {
+                query = @"
+            SELECT MaBaoCaoThang, NgayThang, TenMonAn, DonViTinh, SoLuong, DoanhThuThang 
+            FROM BaoCaoThang
+            WHERE NgayThang BETWEEN @TuNgay AND @DenNgay";
+            }
+
+            DataTable dt = Database.GetData(query, parameters);
+            dgvBaoCao.DataSource = dt;
+
+            // Hiển thị tổng doanh thu
+            decimal tong = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                if (rdoNgay.Checked)
+                    tong += Convert.ToDecimal(row["DoanhThuNgay"]);
+                else
+                    tong += Convert.ToDecimal(row["DoanhThuThang"]);
+            }
+            lblTongDoanhThu.Text = "Tổng doanh thu: " + tong.ToString("N0") + " VNĐ";
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            LoadBaoCao();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
         {
 
         }
---
+++ DoAnNhom3/QuanLy/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DoAnNhom3.QuanLy
+{
+    public class Database
+    {
+        //private static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QuanLyBanHangOnline;Integrated Security=True";
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\QuanLyBanHangOnline.mdf;Integrated Security=True";
+        public static DataTable GetData(string query, SqlParameter[] parameters = null)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                if (parameters != null) cmd.Parameters.AddRange(parameters);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+    }
+}
