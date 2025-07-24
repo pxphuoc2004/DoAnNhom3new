c04f7eb15084a41edf4920ac256388a02de12d78
pxphuoc2004 <pxphuoc.dhti16a1cl@sv.uneti.edu.vn>
2025-07-24 23:34:03 +0700

    Update Program.cs

--- DoAnNhom3/Program.cs
+++ DoAnNhom3/Program.cs
@@ -15,7 +15,7 @@ namespace DoAnNhom3
             Application.SetCompatibleTextRenderingDefault(false);
 
             // Mở trực tiếp form NhanVien (form chính của bạn)
-            Application.Run(new NhanVien());
+            Application.Run(new DangNhap());
         }
     }
 }
